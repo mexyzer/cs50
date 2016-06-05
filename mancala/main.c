5350575bcf6bcc5cd8127f01077b1f8ea4a4a05f
aaron basch <aaronbasch@aarons-mbp.home>
2016-06-04 23:59:06 -0400

    Initial Commit

---
+++ mancala/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mancala
+//
+//  Created by aaron basch on 6/4/16.
+//  Copyright © 2016 aaron basch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
